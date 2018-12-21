de921aa24f8f49c9363ae4ce8365bbc888a3dc5c
Justin Collantes <justincis@outlook.com>
2018-12-21 02:33:45 -0500

    Updates

---
+++ precache-manifest.f6b7424be282b191b44c6097d914bf54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bootstrap-ui-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3833dc470b97be9bcf7",
+    "url": "/bootstrap-ui-react/static/js/runtime~main.a3833dc4.js"
+  },
+  {
+    "revision": "126486220ba8b9044638",
+    "url": "/bootstrap-ui-react/static/js/main.12648622.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/bootstrap-ui-react/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "126486220ba8b9044638",
+    "url": "/bootstrap-ui-react/static/css/main.a40a73bd.chunk.css"
+  },
+  {
+    "revision": "288e6d0e707fd8cfcff127616502f94f",
+    "url": "/bootstrap-ui-react/index.html"
+  }
+];
